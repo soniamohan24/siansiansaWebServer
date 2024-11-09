dfecb1de42b055a31c3af5b3a67eae6536233937
soniamohan24 <soniamohan.ie@gmail.com>
2024-11-09 22:02:33 +0000

    Changes done in the CI/CD pipeline file

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-# gfghddghdhgdhdd
+# gfghddghdhgdhddfff
 if __name__ == '__main__':
     app.run(debug=True)
