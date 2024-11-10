a53ab720253de2924cc257e1d94b46a5eb648ae5
soniamohan24 <soniamohan.ie@gmail.com>
2024-11-10 01:10:14 +0000

    Access denied issue : docker image

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-# gfghddghdggghgdhddfffffgggg
+# gfghddghdggghgdhddfffffggggiii
 if __name__ == '__main__':
     app.run(debug=True)
 #sdfgbhj
