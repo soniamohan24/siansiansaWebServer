f1a5c0815c039f134d2a29ac91644518ac992755
System Administrator <root@Sonias-MacBook-Air.local>
2024-11-10 12:46:14 +0000

    Updated version of code

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-# gfghddghdggghgdhddfffffddsssssaaa
+# gfghddghdggghgdhddfffffddsssssaaahgg
 if __name__ == '__main__':
     app.run(debug=True)
